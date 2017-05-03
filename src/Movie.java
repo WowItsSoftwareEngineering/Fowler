7320f2a48271e2161af2f0437d5bda1e4de65830
WowItsSoftwareEngineering <lordluegen@gmail.com>
2017-05-03 10:48:01 +0200

    Refactored - Fixed argument naming and casing

--- src/Movie.java
+++ src/Movie.java
@@ -4,15 +4,15 @@ public class Movie {
     public static final int NEW_RELEASE = 1;
     private String title;
     private int priceCode;
-    public Movie(String newtitle, int newpriceCode) {
-        title = newtitle;
-        this.setPriceCode(newpriceCode);
+    public Movie(String newTitle, int newPriceCode) {
+        title = newTitle;
+        this.setPriceCode(newPriceCode);
     }
     public int getPriceCode() {
         return priceCode;
     }
-    public void setPriceCode(int arg) {
-        priceCode = arg;
+    public void setPriceCode(int priceCode) {
+        this.priceCode = priceCode;
     }
     public String getTitle (){
         return title;
